3a17ae7dd0b413828577358023e4c494e73bd023
Junade Ali <junade@cloudflare.com>
2016-09-23 18:14:55 +0100

    Refactoring.

---
+++ tests/CryptoLibTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * User: junade
+ * Date: 23/09/2016
+ * Time: 18:03
+ */
+class CryptoLibTest extends PHPUnit_Framework_TestCase
+{
+    public function testChangePepper()
+    {
+
+    }
+}
